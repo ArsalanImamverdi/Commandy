e98698598353e298cf5f3d543cf3cab26503ed30
MoamadArsalan Imamverdi <emamverdi@utravs.com>
2024-10-17 13:15:27 +0330

    test: add test cases

--- Commandy.Tests/WindowsCommandTests.cs
+++ Commandy.Tests/WindowsCommandTests.cs
@@ -1,18 +1,58 @@
-﻿using Commandy.Internals;
-
-namespace Commandy.Tests
+﻿namespace Commandy.Tests
 {
     public class WindowsCommandTests
     {
         [Fact]
-        public void RunCommand_WithShell_WhenSuccess_ShouldSuccess()
+        public async Task RunCommand_WithShell_WhenSuccess_ShouldExitCodeBeZero()
         {
             var command = Command.CreateCommand("dir");
+            command.OnDataReceived += (sender, args) =>
+            {
+                Console.WriteLine(args.Data);
+            };
+            var result = await command.ExecuteAsync();
+
+            Assert.Equal(0, result.ExitCode);
+        }
+
+        [Fact]
+        public void RunCommand_WithoutShell_NoneAsync_WhenSuccess_ShouldExitCodeBeZero()
+        {
+            var command = Command.CreateCommand("git", opt => opt.UseShell(false).AddArgument("--version"));
+            command.OnDataReceived += (sender, args) =>
+            {
+                Console.WriteLine(args.Data);
+            };
             var result = command.Execute();
 
             Assert.Equal(0, result.ExitCode);
         }
 
+        [Fact]
+        public void RunCommand_WithShell_WhenCancel_ShouldExitCodeBeLessThanZeroAndContainsContentBeforExit()
+        {
+            var command = Command.CreateCommand("echo.bat");
+            var cancellation = new CancellationTokenSource();
+            cancellation.CancelAfter(6000);
+
+            var result = command.Execute(cancellation.Token);
+
+            Assert.Equal(-1, result.ExitCode);
+            Assert.Contains("First", result.Output);
+            Assert.Contains("Second", result.Output);
+            Assert.DoesNotContain("Third", result.Output);
+        }
+
+        [Fact]
+        public void RunCommand_WithShell_WhenHasError_ShouldExitCodeBeGreaterThanZeroAndHasErrorAndNoOutput()
+        {
+            var command = Command.CreateCommand("something-cause-error");
+
+            var result = command.Execute();
+            Assert.True(result.ExitCode > 0);
+            Assert.Empty(result.Output);
+            Assert.NotEmpty(result.Error);
+        }
 
     }
 }
---
+++ Commandy.Tests/echo.bat
@@ -0,0 +1,12 @@
+@echo off
+echo First
+timeout /t 1
+echo Second
+timeout /t 6
+echo Third
+timeout /t 10
+echo Forth
+timeout /t 5
+echo Fifth
+timeout /t 10
+
